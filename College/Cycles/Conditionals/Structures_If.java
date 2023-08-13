55ec3eb01bd3cc8863a9606614199ce481dbd975
RepIngSE <139818962+RepIngSE@users.noreply.github.com>
2023-08-13 16:01:32 -0500

    Create

---
+++ College/Cycles/Conditionals/Structures_If.java
@@ -0,0 +1,15 @@
+public class Sicondicional {
+ 
+  
+    public static void main(String[] args) {
+        int i = 20;
+
+        if (i > 15){
+            System.out.println("10 es menor que 15");
+
+        // Esta declaración se ejecutará
+        // como si considerara una declaración por defecto
+        System.out.println("No estoy en IF");
+        }
+    }
+}
