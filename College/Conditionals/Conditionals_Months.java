ea9ce1487bdcf23f9dc1838560913780c6e2c9d0
RepIngSE <139818962+RepIngSE@users.noreply.github.com>
2023-08-13 16:15:43 -0500

    Create

---
+++ College/Conditionals/Conditionals_Months.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+ 
+
+public class Ejerciciodiasdelmes {
+
+    public static void main(String[] args) {
+     int Enero=1, Febrero=2, Marzo=3, Abril=4, Mayo=5, Junio=6, Julio=7, Agosto=8, Septiembre=9, Octubre=10, Noviembre=11, Diciembre=12;
+     int dias,mes;  
+               
+     System.out.println("Digite el mes ");
+     Scanner teclado = new Scanner(System.in);
+     mes=teclado.nextInt();
+
+        while(mes<=12){
+            if(mes==Enero){
+                System.out.println("El mes de Enero tiene en total 31 dias");
+                for (dias=1;dias<=31;dias++){
+        System.out.println(""+dias);
+        
+                }
+    }
+             if(mes==Febrero){
+                System.out.println("El mes de Febrero tiene en total 28 dias");
+                for (dias=1;dias<=28;dias++){
+        System.out.println(""+dias);
+                     }
+    }
+             if(mes==Marzo){
+                System.out.println("El mes de Marzo tiene en total 31 dias");
+                for (dias=1;dias<=31;dias++){
+        System.out.println(""+dias);
+                     }
+    }
+             if(mes==Abril){
+                System.out.println("El mes de Abril tiene en total 30 dias");
+                for (dias=1;dias<=30;dias++){
+        System.out.println(""+dias);
+                     }
+    }
+             if(mes==Mayo){
+                System.out.println("El mes de Mayo tiene en total 31 dias");
+                for (dias=1;dias<=31;dias++){
+        System.out.println(""+dias);
+                     }
+     }
+             
+             if(mes==Junio){
+                System.out.println("El mes de Junio tiene en total 30 dias");
+                for (dias=1;dias<=30;dias++){
+        System.out.println(""+dias);
+                     }
+   
+    }
+            if(mes==Julio){
+                System.out.println("El mes de Julio tiene en total 31 dias");
+                for (dias=1;dias<=31;dias++){
+        System.out.println(""+dias);
+                     }
+   
+    }   
+             if(mes==Agosto){
+                System.out.println("El mes de Agosto tiene en total 31 dias");
+                for (dias=1;dias<=31;dias++){
+        System.out.println(""+dias);
+                     }
+   
+    }           
+             if(mes==Septiembre){
+                System.out.println("El mes de Septiembre tiene en total 30 dias");
+                for (dias=1;dias<=30;dias++){
+        System.out.println(""+dias);
+                     }
+   
+    }
+             if(mes==Octubre){
+                System.out.println("El mes de Octubre tiene en total 31 dias");
+                for (dias=1;dias<=31;dias++){
+        System.out.println(""+dias);
+                     }
+   
+    }
+             if(mes==Noviembre){
+                System.out.println("El mes de Noviembre tiene en total 30 dias");
+                for (dias=1;dias<=30;dias++){
+        System.out.println(""+dias);
+                      }
+   
+    }
+             if(mes==Diciembre){
+                System.out.println("El mes de Diciembre tiene en total 31 dias");
+                for (dias=1;dias<=31;dias++){
+        System.out.println(""+dias);
+                      }
+    }
+        mes=13; 
+       }
+    }
+ }
